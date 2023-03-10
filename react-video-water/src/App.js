c16c6608e6e45634586917624a095b956981adc4
Esteban Fuhrmann ( Colo ) <esteban.informatica@gmail.com>
2023-03-10 15:02:04 -0300

    ff + design

--- react-video-water/src/App.js
+++ react-video-water/src/App.js
@@ -1,65 +1,77 @@
-// import React, { useState } from 'react';
-// import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
-// import './App.css';
-
-// function App() {
-//   const [videoSrc, setVideoSrc] = useState('');
-//   const [message, setMessage] = useState('Click Start to transcode');
-//   const ffmpeg = createFFmpeg({
-//     log: true,
-//   });
-//   const doTranscode = async () => {
-//     setMessage('Loading ffmpeg-core.js');
-//     await ffmpeg.load();
-//     setMessage('Start transcoding');
-//     ffmpeg.FS('writeFile', 'test.avi', await fetchFile('/flame.avi'));
-//     await ffmpeg.run('-i', 'test.avi', 'test.mp4');
-//     setMessage('Complete transcoding');
-//     const data = ffmpeg.FS('readFile', 'test.mp4');
-//     setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })));
-//   };
-//   return (
-//     <div className="App">
-//       <p/>
-//       <video src={videoSrc} controls></video><br/>
-//       <button onClick={doTranscode}>Start</button>
-//       <p>{message}</p>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React, { useState } from 'react';
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import { Button, Container, Typography } from '@material-ui/core';
+import { CloudUpload as CloudUploadIcon, Movie as MovieIcon } from '@material-ui/icons';
 import './App.css';
 
 function App() {
+  const [videoFile, setVideoFile] = useState(null);
+  const [logoFile, setLogoFile] = useState(null);
   const [videoSrc, setVideoSrc] = useState('');
-  const [message, setMessage] = useState('Click Start to transcode');
+  const [message, setMessage] = useState('Click To Add Logo');
   const ffmpeg = createFFmpeg({
     log: true,
   });
+
+  const handleVideoChange = (event) => {
+    setVideoFile(event.target.files?.[0]);
+  };
+
+  const handleLogoChange = (event) => {
+    setLogoFile(event.target.files?.[0]);
+  };
+
   const doTranscode = async () => {
     setMessage('Loading ffmpeg-core.js');
     await ffmpeg.load();
-    setMessage('Start transcoding');
-    ffmpeg.FS('writeFile', 'input.mp4', await fetchFile('/fa.mp4'));
-    ffmpeg.FS('writeFile', 'logo.png', await fetchFile('/logo192.png'));
+    setMessage('In progress...');
+    ffmpeg.FS('writeFile', 'input.mp4', await fetchFile(videoFile));
+    ffmpeg.FS('writeFile', 'logo.png', await fetchFile(logoFile));
     await ffmpeg.run('-i', 'input.mp4', '-i', 'logo.png', '-filter_complex', 'overlay=10:10', 'output.mp4');
-    setMessage('Complete transcoding');
+    setMessage('Complete video processing');
     const data = ffmpeg.FS('readFile', 'output.mp4');
     setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })));
   };
+
   return (
-    <div className="App">
-      <p />
+    <Container maxWidth="sm" className="App">
+      <Typography variant="h4" component="h1" gutterBottom>
+        Add Logo to Video
+      </Typography>
       <video src={videoSrc} controls />
       <br />
-      <button onClick={doTranscode}>Start</button>
-      <p>{message}</p>
-    </div>
+      <Button
+        variant="contained"
+        color="primary"
+        component="label"
+        startIcon={<MovieIcon />}
+      >
+        Upload Video
+        <input type="file" hidden onChange={handleVideoChange} />
+      </Button>
+      &nbsp;
+      <Button
+        variant="contained"
+        color="secondary"
+        component="label"
+        startIcon={<CloudUploadIcon />}
+      >
+        Upload Logo
+        <input type="file" hidden onChange={handleLogoChange} />
+      </Button>
+      <br /><br />
+      <Button
+        variant="contained"
+        color="primary"
+        disabled={!videoFile || !logoFile}
+        onClick={doTranscode}
+      >
+        Start
+      </Button>
+      <Typography variant="body1" component="p" gutterBottom>
+        {message}
+      </Typography>
+    </Container>
   );
 }
 
