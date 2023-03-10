3d53a6725dadc84cdfd53c8f5b481839fbec3c8c
Esteban Fuhrmann ( Colo ) <esteban.informatica@gmail.com>
2023-03-10 14:33:15 -0300

    ffmpeg in browser

---
+++ react-video-water/src/App.js
@@ -0,0 +1,66 @@
+// import React, { useState } from 'react';
+// import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+// import './App.css';
+
+// function App() {
+//   const [videoSrc, setVideoSrc] = useState('');
+//   const [message, setMessage] = useState('Click Start to transcode');
+//   const ffmpeg = createFFmpeg({
+//     log: true,
+//   });
+//   const doTranscode = async () => {
+//     setMessage('Loading ffmpeg-core.js');
+//     await ffmpeg.load();
+//     setMessage('Start transcoding');
+//     ffmpeg.FS('writeFile', 'test.avi', await fetchFile('/flame.avi'));
+//     await ffmpeg.run('-i', 'test.avi', 'test.mp4');
+//     setMessage('Complete transcoding');
+//     const data = ffmpeg.FS('readFile', 'test.mp4');
+//     setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })));
+//   };
+//   return (
+//     <div className="App">
+//       <p/>
+//       <video src={videoSrc} controls></video><br/>
+//       <button onClick={doTranscode}>Start</button>
+//       <p>{message}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { useState } from 'react';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import './App.css';
+
+function App() {
+  const [videoSrc, setVideoSrc] = useState('');
+  const [message, setMessage] = useState('Click Start to transcode');
+  const ffmpeg = createFFmpeg({
+    log: true,
+  });
+  const doTranscode = async () => {
+    setMessage('Loading ffmpeg-core.js');
+    await ffmpeg.load();
+    setMessage('Start transcoding');
+    ffmpeg.FS('writeFile', 'input.mp4', await fetchFile('/fa.mp4'));
+    ffmpeg.FS('writeFile', 'logo.png', await fetchFile('/logo192.png'));
+    await ffmpeg.run('-i', 'input.mp4', '-i', 'logo.png', '-filter_complex', 'overlay=10:10', 'output.mp4');
+    setMessage('Complete transcoding');
+    const data = ffmpeg.FS('readFile', 'output.mp4');
+    setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })));
+  };
+  return (
+    <div className="App">
+      <p />
+      <video src={videoSrc} controls />
+      <br />
+      <button onClick={doTranscode}>Start</button>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
